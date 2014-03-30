74bf07af9962b8abe654adb61ef88b8d94baa1c6
nmercer <nm3rc3r@gmail.com>
2014-03-30 17:04:11 +0000

    Moar

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 BUTTON_PIN = 25
 LIGHT_PIN = 22
@@ -9,14 +12,19 @@ GPIO.setup(BUTTON_PIN, GPIO.IN)
 GPIO.setup(LIGHT_PIN, GPIO.OUT)
 
 prev_input = 1
+
 while True:
   input = GPIO.input(BUTTON_PIN)
 
   if prev_input != input and not input:
+    start = datetime.now()
     print("Button Pushed DOWN")
     GPIO.output(LIGHT_PIN, GPIO.HIGH)
   elif prev_input != input and input:
-    print("Button Released")
+    time_pressed = (start-datetime.now()).total_seconds()
     GPIO.output(LIGHT_PIN, GPIO.LOW)
+    print("Button Released")
+    print('Seconds Pressed: %s' % time_pressed)
+
   prev_input = input
   time.sleep(0.05)
